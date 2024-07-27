399a8bfce3788902cff499f8b776fa3c7b7a6227
Tubers <l.j.c.g.3.3.3@gmail.com>
2024-07-27 11:44:40 -0700

    profile.ps1 init

---
+++ helpers/profile.ps1
@@ -0,0 +1,7 @@
+oh-my-posh init pwsh --config 'C:\Program Files (x86)\oh-my-posh\themes\velvet.omp.json' | Invoke-Expression
+set-location -path $env:Userprofile\desktop
+$env:PYTHONUNBUFFERED = "1"
+Clear-Host
+Set-Alias vim nvim
+
+
